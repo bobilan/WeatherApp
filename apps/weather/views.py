3452808be662cbcbe6e6f9f86aef668603a678fb
bobilan <bobilan@gmail.com>
2023-09-01 15:44:24 +0200

    Adjust html

--- apps/weather/views.py
+++ apps/weather/views.py
@@ -13,11 +13,17 @@ def weather_view(request):
             'temperature': 26,
             'humidity': 59,
             'wind_speed': 0.5,
-            'weather_icon': '04d'
+            'weather_icon': '04d',
+            'country': 'UA'
         }
+        clothing_data = {
+            'clothing': 't-shirt, light jacket, light sweater, linen trousers, shorts ',
+            'shoes': 'sandals, flip-flops, loafers, sneakers, trainers ',
+            'accessories': 'sunglasses, brimmed hat, light scarf'}
 
         # Store the weather_data dictionary in the session
         request.session['weather_data'] = weather_data
+        request.session['clothing_data'] = clothing_data
 
         return redirect('second_view')
     return render(request, 'weather/index.html')
@@ -26,13 +32,14 @@ def weather_view(request):
 def second_view(request):
     # Retrieve the weather_data dictionary from the session
     weather_data = request.session.get('weather_data', {})
+    clothing_data = request.session.get('clothing_data', "")
 
     if not weather_data:
         # Handle the case where weather_data is not found in the session
         messages.error(request, 'Weather data not found in session.')
         return redirect('weather/index.html')  # Redirect to another view or page
 
-    return render(request, 'weather/second.html', {'weather_data': weather_data})
+    return render(request, 'weather/second.html', {'weather_data': weather_data, "clothing_data": clothing_data})
 
 
 
