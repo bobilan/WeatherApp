a116ffa00af18fd43c5c72421bf51af7c6fe05c7
bobilan <bobilan@gmail.com>
2023-09-13 15:51:26 +0200

    change button placeholder

--- apps/weather/form.py
+++ apps/weather/form.py
@@ -4,5 +4,5 @@ from django import forms
 class SearchForm(forms.Form):
     search_bar = forms.CharField(
         max_length=100,
-        widget=forms.TextInput(attrs={'placeholder': 'City or zip code'}),
+        widget=forms.TextInput(attrs={'placeholder': 'City or zip'}),
     )
